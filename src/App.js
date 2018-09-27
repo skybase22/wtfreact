b3690f4e872a3e26882a26f845331687a20dc89a
skybase22 <bes-noppadol0147@hotmail.com>
2018-09-28 00:38:03 +0700

    hi

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 
-import './App.css';
+import CharacterCard from './CharacterCard';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello World
- </div>
+      <div>
+        <CharacterCard value="h" />
+        <CharacterCard value="i" />
+      </div>
     );
   }
 }
+
 export default App;
