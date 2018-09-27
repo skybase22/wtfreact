8a2dcdc6e848d48cfb74baaae85e5e1cd61bf617
skybase22 <bes-noppadol0147@hotmail.com>
2018-09-28 00:50:26 +0700

    box2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 
 const word = "Hello";
 
@@ -9,13 +10,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        {
-          Array.from(word).map((c, i) => <CharacterCard value={c} key={i} />)
-        }
+        <WordCard value="hello" />
       </div>
     );
   }
-}
+ }
+
 
 
 export default App;
---
+++ src/WordCard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CharacterCard from './CharacterCard';
+
+export default class WordCard extends
+    Component {
+    render() {
+        return (
+            <div>
+                {Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} />)}
+            </div>
+        );
+    }
+}
